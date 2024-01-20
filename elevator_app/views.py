73f6713ba13e0274ae1cef9d999705cfe784e7e5
Shivansh Chaudhary <shivanshchaudhary18@gmail.com>
2024-01-21 00:08:46 +0530

    Added serializers, viewsets, and URL routing for Elevator and Request models

--- elevator_app/views.py
+++ elevator_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Elevator, Request
+from .serializers import ElevatorSerializer, RequestSerializer
+
+class ElevatorViewSet(viewsets.ModelViewSet):
+    queryset = Elevator.objects.all()
+    serializer_class = ElevatorSerializer
+
+class RequestViewSet(viewsets.ModelViewSet):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+
